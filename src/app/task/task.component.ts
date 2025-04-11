8448d942734fca3d5b127f6d86d26d0de3882711
ShanihaS <shanihacse2025@gmail.com>
2025-04-11 12:11:13 +0530

    added

---
+++ src/app/task/task.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import Chart from 'chart.js/auto';  
+
+// and then paste the same chart config as above.
+
+@Component({
+  selector: 'app-task',
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+ 
+}
